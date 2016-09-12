d76209cf8a82c8386338e4bccd10caf4f41f8c52
Sam Pohlenz <sam@sampohlenz.com>
2016-09-12 10:53:40 +0930

    Render default dashboard template when no admins are found

--- app/controllers/trestle/dashboard_controller.rb
+++ app/controllers/trestle/dashboard_controller.rb
@@ -1,14 +1,15 @@
 class Trestle::DashboardController < Trestle::ApplicationController
   def index
-    redirect_to primary_admin.path
+    admin = primary_admin
+    redirect_to admin.path if admin
   end
 
 private
   def primary_admin
     if navigation = Trestle.navigation.first
       navigation.admin
-    elsif Trestle.admin.values.any?
-      Trestle.admin.values.first
+    elsif Trestle.admins.values.any?
+      Trestle.admins.values.first
     end
   end
 end
