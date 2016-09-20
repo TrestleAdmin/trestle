461a8e83b91d554d4c6671b9d623c415ce9f8818
Sam Pohlenz <sam@sampohlenz.com>
2016-09-20 19:40:37 +0930

    Refactor sort_link helper

--- app/helpers/trestle/sort_helper.rb
+++ app/helpers/trestle/sort_helper.rb
@@ -1,28 +1,55 @@
 module Trestle
   module SortHelper
     def sort_link(text, field, options={})
-      default_order = options.fetch(:default_order, "asc").to_s
-
-      active = params[:sort] == field.to_s
-
-      classes = ["sort"]
-
-      if active
-        order = (params[:order] == "asc") ? "desc" : "asc"
-        current_order = (default_order == "desc") ? order : reverse_order(order)
-
-        classes << "sort-#{current_order}"
-        classes << "active"
-      else
-        order = default_order
-      end
-
-      link_to text, params.permit(:sort, :order, :q).merge(sort: field, order: order), class: classes
+      sort_link = SortLink.new(field, params, options)
+      link_to text, sort_link.params, class: sort_link.classes
     end
 
-  private
-    def reverse_order(order)
-      order == "asc" ? "desc" : "asc"
+    class SortLink
+      attr_reader :field
+
+      def initialize(field, params, options)
+        @field, @params, @options = field, params, options
+      end
+
+      def active?
+        @params[:sort] == field.to_s
+      end
+
+      def params
+        @params.permit(:sort, :order, :q).merge(sort: field, order: order)
+      end
+
+      def order
+        if active?
+          reverse_order(@params[:order] || "desc")
+        else
+          default_order
+        end
+      end
+
+      def current_order
+        default_order == "desc" ? order : reverse_order(order)
+      end
+
+      def default_order
+        @options.fetch(:default_order, "asc").to_s
+      end
+
+      def classes
+        classes = ["sort"]
+
+        if active?
+          classes << "sort-#{current_order}"
+          classes << "active"
+        end
+
+        classes
+      end
+
+      def reverse_order(order)
+        order == "asc" ? "desc" : "asc"
+      end
     end
   end
 end
