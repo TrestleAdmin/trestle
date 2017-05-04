a32b29efa1e24ada6f1280d6694e7adad5e6000d
Sam Pohlenz <sam@sampohlenz.com>
2017-05-04 11:51:11 +0930

    Include default actions in automatic table columns

--- lib/trestle/table/automatic.rb
+++ lib/trestle/table/automatic.rb
@@ -7,9 +7,17 @@ module Trestle
       end
 
       def columns
+        content_columns + [actions_column]
+      end
+
+      def content_columns
         @admin.default_columns.map.with_index do |(name, type), index|
           Column.new(self, name.to_sym, link: index.zero?, align: ("center" if [:datetime, :time, :date].include?(type)))
         end
+      end
+
+      def actions_column
+        ActionsColumn.new(self)
       end
     end
   end
