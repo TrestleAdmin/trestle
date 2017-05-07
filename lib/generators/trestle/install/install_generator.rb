81afc3a5a8187ed11dc1080b525c2c0df8eba20c
Sam Pohlenz <sam@sampohlenz.com>
2017-05-07 15:00:08 +0930

    Create trestle:install generator

---
+++ lib/generators/trestle/install/install_generator.rb
@@ -0,0 +1,23 @@
+module Trestle
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      desc "Installs Trestle and creates files for configuration and asset customization"
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      def create_initializer
+        template "trestle.rb.erb", "config/initializers/trestle.rb"
+      end
+
+      def create_assets
+        template "_variables.scss",   "app/assets/stylesheets/trestle/_variables.scss"
+        template "_custom.scss",      "app/assets/stylesheets/trestle/_custom.scss"
+        template "_custom.js.coffee", "app/assets/javascripts/trestle/_custom.js.coffee"
+      end
+
+      def create_directory
+        empty_directory "app/admin"
+      end
+    end
+  end
+end
