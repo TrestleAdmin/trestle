1678eb01cde2941a12d87671c785146d26cfbfd7
Sam Pohlenz <sam@sampohlenz.com>
2017-05-14 09:39:13 +0930

    Set default rows on textarea fields

--- lib/trestle/form/fields/text_area.rb
+++ lib/trestle/form/fields/text_area.rb
@@ -1,4 +1,8 @@
 class Trestle::Form::Fields::TextArea < Trestle::Form::Fields::FormControl
+  def defaults
+    super.merge(rows: 5)
+  end
+
   def field
     builder.raw_text_area(name, options)
   end
