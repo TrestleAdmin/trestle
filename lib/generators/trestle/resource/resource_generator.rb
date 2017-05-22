d25fc28638f70f2f508d4396efc42cbd0ae7b6fd
Sam Pohlenz <sam@sampohlenz.com>
2017-05-22 22:37:02 +0930

    Create new resource generator

---
+++ lib/generators/trestle/resource/resource_generator.rb
@@ -0,0 +1,22 @@
+module Trestle
+  module Generators
+    class ResourceGenerator < ::Rails::Generators::NamedBase
+      desc "Creates a Trestle admin resource"
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      def create_admin
+        template "admin.rb.erb", File.join('app/admin', class_path, "#{plural_name}_admin.rb")
+      end
+
+    protected
+      def module_name
+        class_name.deconstantize
+      end
+
+      def module?
+        module_name.present?
+      end
+    end
+  end
+end
