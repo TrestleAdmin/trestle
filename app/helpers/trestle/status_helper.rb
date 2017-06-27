8005cae8ad38d939848d1f9fd4fe0f80a55dbbc8
Sam Pohlenz <sam@sampohlenz.com>
2017-06-27 16:43:01 +0930

    Allow status_tag helper to accept options hash

--- app/helpers/trestle/status_helper.rb
+++ app/helpers/trestle/status_helper.rb
@@ -1,7 +1,8 @@
 module Trestle
   module StatusHelper
-    def status_tag(label, status=:default)
-      content_tag(:span, label, class: ["label", "label-#{status}"])
+    def status_tag(label, status=:default, options={})
+      options[:class] ||= ["label", "label-#{status}"]
+      content_tag(:span, label, options)
     end
   end
 end
