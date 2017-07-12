016b6ca5051ed264c08e24e0fea974d07757828f
Sam Pohlenz <sam@sampohlenz.com>
2017-07-12 16:56:15 +0930

    Don't automatically wrap gravatar in avatar block

--- app/helpers/trestle/avatar_helper.rb
+++ app/helpers/trestle/avatar_helper.rb
@@ -1,11 +1,11 @@
 module Trestle
   module AvatarHelper
-    def gravatar(email)
-      avatar { image_tag("https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.to_s.downcase)}.png") }
-    end
-
     def avatar(&block)
       content_tag(:div, class: "avatar", &block)
+    end
+
+    def gravatar(email)
+      image_tag("https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.to_s.downcase)}.png")
     end
   end
 end
