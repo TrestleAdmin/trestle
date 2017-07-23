31226b3507bbda658f27b372db2cc9afe31720a6
Sam Pohlenz <sam@sampohlenz.com>
2017-07-23 17:42:01 +0930

    Add I18n defaults for flash messages

--- lib/trestle/resource/controller.rb
+++ lib/trestle/resource/controller.rb
@@ -13,10 +13,10 @@ module Trestle
         self.instance = admin.build_instance(admin.permitted_params(params))
 
         if admin.save_instance(instance)
-          flash[:message] = flash_message("success.create")
+          flash[:message] = flash_message("success.create", "The %{model_name} was successfully created.")
           redirect_to action: :show, id: admin.to_param(instance)
         else
-          flash.now[:error] = flash_message("failure.create")
+          flash.now[:error] = flash_message("failure.create", default: "Please correct the errors below.")
           render "new"
         end
       end
@@ -34,10 +34,10 @@ module Trestle
         admin.update_instance(instance, admin.permitted_params(params))
 
         if admin.save_instance(instance)
-          flash[:message] = flash_message("success.update")
+          flash[:message] = flash_message("success.update", default: "The %{model_name} was successfully updated.")
           redirect_to action: :show, id: admin.to_param(instance)
         else
-          flash.now[:error] = flash_message("failure.update")
+          flash.now[:error] = flash_message("failure.update", default: "Please correct the errors below.")
           render "show"
         end
       end
@@ -46,9 +46,9 @@ module Trestle
         self.instance = admin.find_instance(params)
 
         if admin.delete_instance(instance)
-          flash[:message] = flash_message("success.destroy")
+          flash[:message] = flash_message("success.destroy", default: "The %{model_name} was successfully deleted.")
         else
-          flash[:error] = flash_message("failure.destroy")
+          flash[:error] = flash_message("failure.destroy", default: "Could not delete %{model_name}.")
         end
 
         redirect_to action: :index
@@ -61,8 +61,8 @@ module Trestle
       attr_accessor :instance
       helper_method :instance
 
-      def flash_message(type)
-        t("trestle.flash.#{type}", model_name: admin.model_name.underscore.humanize(capitalize: false))
+      def flash_message(type, options={})
+        t("trestle.flash.#{type}", options.merge(model_name: admin.model_name.underscore.humanize(capitalize: false)))
       end
     end
   end
