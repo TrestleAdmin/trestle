03e3a3c916a6307c3782aa7f766ad37231c1cf08
Sam Pohlenz <sam@sampohlenz.com>
2017-08-03 11:08:57 +0930

    Set dashboard route to lower priority than admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Trestle::Engine.routes.draw do
-  root to: "trestle/dashboard#index"
-  
   Trestle.admins.each do |name, admin|
     instance_eval(&admin.routes)
   end
+
+  root to: "trestle/dashboard#index"
 end
