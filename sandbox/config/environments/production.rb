c8ede4afa25107087abd54b4e9b78608524b9771
Sam Pohlenz <sam@sampohlenz.com>
2017-08-10 11:46:36 +0930

    Remove ActionMailer/ActionMailer config from sandbox app

--- sandbox/config/environments/production.rb
+++ sandbox/config/environments/production.rb
@@ -55,11 +55,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dummy_#{Rails.env}"
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
