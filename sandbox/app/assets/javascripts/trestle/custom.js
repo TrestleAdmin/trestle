539204bdf22d074eb434cbcc97ffa089e74217ec
Sam Pohlenz <sam@sampohlenz.com>
2017-08-14 20:06:08 +0930

    Convert CoffeeScript in sandbox to JavaScript

---
+++ sandbox/app/assets/javascripts/trestle/custom.js
@@ -0,0 +1,7 @@
+// This file may be used for providing additional customizations to the Trestle
+// admin. It will be automatically included within all admin pages.
+//
+// For organizational purposes, you may wish to define your customizations
+// within individual partials and `require` them here.
+//
+//  e.g. //= require "trestle/custom/my_custom_js"
