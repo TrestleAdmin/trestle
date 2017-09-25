76f45f6f57c33af2e6f0c9a7b7375d83c6110c3c
Sam Pohlenz <sam@sampohlenz.com>
2017-09-25 14:09:38 +0930

    Fix Trestle::Options.merge! with nested hashes

--- lib/trestle/options.rb
+++ lib/trestle/options.rb
@@ -4,14 +4,14 @@ module Trestle
       self[hash]
     end
 
-    def merge(other)
-      dup.merge!(other)
+    def merge(other, &block)
+      dup.merge!(other, &block)
     end
 
-    def merge!(other)
+    def merge!(other, &block)
       super(other || {}) do |key, v1, v2|
         if v1.is_a?(Hash) && v2.is_a?(Hash)
-          v1.deep_merge(v2, &block)
+          v1.merge(v2, &block)
         elsif v1.is_a?(Array)
           v1 + Array(v2)
         else
--- spec/trestle/options_spec.rb
+++ spec/trestle/options_spec.rb
@@ -1,44 +1,61 @@
 require 'spec_helper'
 
 describe Trestle::Options do
-  subject(:options) { Trestle::Options.new(singular: "foo", array: [1, 2, 3]) }
+  subject(:options) { Trestle::Options.new }
 
   it "is a hash" do
     expect(options).to be_a(Hash)
   end
 
   describe "#merge" do
-    it "returns a Trestle::Options instance" do
-      expect(options.merge({})).to be_a(Trestle::Options)
-    end
-
     it "overrides singular values" do
-      expect(options.merge(singular: "changed")).to eq(Trestle::Options.new(singular: "changed", array: [1, 2, 3]))
+      options = Trestle::Options.new(singular: "original")
+      expect(options.merge(singular: "changed")).to eq(Trestle::Options.new(singular: "changed"))
     end
 
     it "appends arrays to array values" do
-      expect(options.merge(array: [4, 5, 6])).to eq(Trestle::Options.new(singular: "foo", array: [1, 2, 3, 4, 5, 6]))
+      options = Trestle::Options.new(array: [1, 2, 3])
+      expect(options.merge(array: [4, 5, 6])).to eq(Trestle::Options.new(array: [1, 2, 3, 4, 5, 6]))
     end
 
     it "appends singular values to array values" do
-      expect(options.merge(array: 4)).to eq(Trestle::Options.new(singular: "foo", array: [1, 2, 3, 4]))
+      options = Trestle::Options.new(array: [1, 2, 3])
+      expect(options.merge(array: 4)).to eq(Trestle::Options.new(array: [1, 2, 3, 4]))
+    end
+
+    it "merges nested hashes" do
+      options = Trestle::Options.new(hash: { a: 123, b: "456" })
+      expect(options.merge(hash: { b: "abc", c: "def" })).to eq(Trestle::Options.new(hash: { a: 123, b: "abc", c: "def" }))
     end
   end
 
   describe "#merge!" do
     it "overrides singular values" do
+      options = Trestle::Options.new(singular: "original")
       options.merge!(singular: "changed")
-      expect(options).to eq(Trestle::Options.new(singular: "changed", array: [1, 2, 3]))
+
+      expect(options).to eq(Trestle::Options.new(singular: "changed"))
     end
 
     it "appends arrays to array values" do
+      options = Trestle::Options.new(array: [1, 2, 3])
       options.merge!(array: [4, 5, 6])
-      expect(options).to eq(Trestle::Options.new(singular: "foo", array: [1, 2, 3, 4, 5, 6]))
+
+      expect(options).to eq(Trestle::Options.new(array: [1, 2, 3, 4, 5, 6]))
     end
 
     it "appends singular values to array values" do
+      options = Trestle::Options.new(array: [1, 2, 3])
       options.merge!(array: 4)
-      expect(options).to eq(Trestle::Options.new(singular: "foo", array: [1, 2, 3, 4]))
+
+      expect(options).to eq(Trestle::Options.new(array: [1, 2, 3, 4]))
+    end
+
+    it "merges nested hashes" do
+      options = Trestle::Options.new(hash: { a: 123, b: "456" })
+      options.merge!(hash: { b: "abc", c: "def" })
+
+      expect(options).to eq(Trestle::Options.new(hash: { a: 123, b: "abc", c: "def" }))
     end
   end
 end
