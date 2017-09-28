6658778fd6b08c9b191d831e75304912d3180e92
Sam Pohlenz <sam@sampohlenz.com>
2017-09-28 12:00:17 +0930

    Focus on first tab containing errors

--- app/assets/javascripts/trestle/components/_tabs.js
+++ app/assets/javascripts/trestle/components/_tabs.js
@@ -10,6 +10,11 @@ Trestle.init(function(e, root) {
 
 Trestle.ready(function() {
   if (location.hash.substr(0, 2) == "#!") {
+    // Focus on active tab from URL
     $("a[data-toggle='tab'][href='#" + location.hash.substr(2) + "']").tab("show");
+  } else if ($(".tab-pane:has(.has-error)").length) {
+    // Focus on first tab with errors
+    var pane = $(".tab-pane:has(.has-error)").first();
+    $("a[data-toggle='tab'][href='#" + pane.attr("id") + "']").tab("show");
   }
 });
