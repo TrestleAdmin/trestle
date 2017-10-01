56ed0aa08a5bc8fb48c76e4917720bb09b2e9df6
Jean Lazarou <jean.lazarou@gmail.com>
2017-10-01 19:16:05 +0200

    Add French localization files

---
+++ config/locales/fr.rb
@@ -0,0 +1,18 @@
+{
+  fr: {
+    date: {
+      formats: {
+        trestle_date: "%e %b %Y",
+        trestle_calendar: "%d-%m-%Y"
+      }
+    },
+
+    time: {
+      formats: {
+        trestle_date: "%e %b %Y",
+        trestle_time: "%Hh%M",
+        trestle_time_with_seconds: '%Hh %M\' %S"'
+      }
+    }
+  }
+}
