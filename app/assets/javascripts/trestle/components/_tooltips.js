0dd6525c83a1060ae1bbfc817a71876caf45a2c0
Sam Pohlenz <sam@sampohlenz.com>
2018-01-29 15:02:00 +1030

    Show tooltips on collapsed navigation on non-touch devices

--- app/assets/javascripts/trestle/components/_tooltips.js
+++ app/assets/javascripts/trestle/components/_tooltips.js
@@ -1,3 +1,14 @@
 Trestle.init(function(e, root) {
   $(root).find('[data-toggle="tooltip"]').tooltip();
 });
+
+if (!('ontouchstart' in window)) {
+  Trestle.ready(function() {
+    $(document).tooltip({
+      selector:  '.app-nav a',
+      trigger:   'hover',
+      placement: 'right',
+      title:     function() { return $(this).find('.nav-label').text(); }
+    });
+  });
+};
