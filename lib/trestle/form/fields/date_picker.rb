991bdb70e1fe7e441b6b74f2c08c81770ab7c25d
Jan Svoboda <jan@mluv.cz>
2018-03-03 22:16:28 +0100

    fix syntax error

--- lib/trestle/form/fields/date_picker.rb
+++ lib/trestle/form/fields/date_picker.rb
@@ -1,7 +1,7 @@
 module Trestle::Form::Fields::DatePicker
   def extract_options!
     options[:prepend] ||= options.key?(:icon) ? options.delete(:icon) : default_icon
-    options.deep_merge!(data: { picker: options.fetch(:picker, true })
+    options.deep_merge!(data: { picker: options.fetch(:picker, true) })
 
     super
   end
