ee105a68f4e45db7e1c8d37422d402be609f534c
Samir Sabri <as3now@gmail.com>
2018-05-20 21:04:43 +0300

    total_count fallback to count safely

--- app/helpers/trestle/pagination_helper.rb
+++ app/helpers/trestle/pagination_helper.rb
@@ -3,15 +3,18 @@ module Trestle
     # Custom version of Kaminari's page_entries_info helper to use a
     # Trestle-scoped I18n key and add a delimiter to the total count.
     def page_entries_info(collection, options = {})
+
+      total_count= collection.try(:total_count) || collection.count
+
       entry_name = options[:entry_name] || "entry"
-      entry_name = entry_name.pluralize unless collection.total_count == 1
+      entry_name = entry_name.pluralize unless total_count == 1
 
       if collection.total_pages < 2
-        t('trestle.helpers.page_entries_info.one_page.display_entries', entry_name: entry_name, count: collection.total_count, default: "Displaying <strong>all %{count}</strong> %{entry_name}")
+        t('trestle.helpers.page_entries_info.one_page.display_entries', entry_name: entry_name, count: total_count, default: "Displaying <strong>all %{count}</strong> %{entry_name}")
       else
         first = number_with_delimiter(collection.offset_value + 1)
-        last  = number_with_delimiter((sum = collection.offset_value + collection.limit_value) > collection.total_count ? collection.total_count : sum)
-        total = number_with_delimiter(collection.total_count)
+        last  = number_with_delimiter((sum = collection.offset_value + collection.limit_value) > total_count ? total_count : sum)
+        total = number_with_delimiter(total_count)
 
         t('trestle.helpers.page_entries_info.more_pages.display_entries', entry_name: entry_name, first: first, last: last, total: total, default: "Displaying %{entry_name} <strong>%{first}&nbsp;-&nbsp;%{last}</strong> of <b>%{total}</b>")
       end.html_safe
