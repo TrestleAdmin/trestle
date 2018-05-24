de460b38568f4f49a3bde65b50f9d4a5d84b4489
Sam Pohlenz <sam@sampohlenz.com>
2018-05-24 10:11:53 +0930

    Run Chrome specs with --no-sandbox option

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,7 +2,7 @@ require 'capybara/rails'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new app, browser: :chrome,
-    options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+    options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu no-sandbox])
 end
 
 Capybara.javascript_driver = :chrome
