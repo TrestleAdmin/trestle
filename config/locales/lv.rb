ad8a9286758412fbd62b8ca2faabcd6e440f2c9d
Martins Kruze <martins.kruze@tieto.com>
2018-07-16 23:13:11 +0300

    Adding of Latvian language translations

---
+++ config/locales/lv.rb
@@ -0,0 +1,18 @@
+{
+  lv: {
+    date: {
+      formats: {
+        trestle_date: "%e %b %Y",
+        trestle_calendar: "%d-%m-%Y"
+      }
+    },
+
+    time: {
+      formats: {
+        trestle_date: "%e %b %Y",
+        trestle_time: "%H:%M",
+        trestle_time_with_seconds: "%H:%M:%S"
+      }
+    }
+  }
+}
