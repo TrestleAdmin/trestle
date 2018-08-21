a1585317d53417aaf06018d55aa1e6787937d671
Sam Pohlenz <sam@sampohlenz.com>
2018-08-21 16:02:57 +0930

    Add general confirmation popup with data-toggle="confirm"

--- app/assets/javascripts/trestle/components/_confirmation.js
+++ app/assets/javascripts/trestle/components/_confirmation.js
@@ -1,3 +1,29 @@
+Trestle.confirmation = {};
+
+Trestle.confirmation.DEFAULTS = {
+  rootSelector:   'body',
+  singleton:      true,
+  popout:         true,
+  title:          Trestle.i18n['admin.confirmation.title'] || 'Are you sure?',
+  btnOkIcon:      '',
+  btnOkClass:     'btn-primary',
+  btnOkLabel:     Trestle.i18n['admin.confirmation.ok'] || 'OK',
+  btnCancelIcon:  '',
+  btnCancelClass: 'btn-default',
+  btnCancelLabel: Trestle.i18n['admin.confirmation.cancel'] || 'Cancel',
+  copyAttributes: ''
+};
+
+Trestle.confirmation.CONFIRM = $.extend({}, Trestle.confirmation.DEFAULTS, {
+  selector: '[data-toggle="confirm"]'
+});
+
+Trestle.confirmation.DELETE = $.extend({}, Trestle.confirmation.DEFAULTS, {
+  selector:   '[data-toggle="confirm-delete"]',
+  btnOkClass: 'btn-danger',
+  btnOkLabel: Trestle.i18n['admin.confirmation.delete'] || 'Delete'
+});
+
 Trestle.ready(function() {
   // This must be bound to an element beneath document so
   // that it is fired before any jquery_ujs events.
@@ -6,18 +32,9 @@ Trestle.ready(function() {
   // Ensure it is only initialized once
   if (root.data('bs.confirmation')) return;
 
-  root.confirmation({
-    rootSelector:   'body',
-    selector:       '[data-toggle="confirm-delete"]',
-    singleton:      true,
-    popout:         true,
-    title:          Trestle.i18n['admin.confirmation.title'] || 'Are you sure?',
-    btnOkIcon:      '',
-    btnOkClass:     'btn-danger',
-    btnOkLabel:     Trestle.i18n['admin.confirmation.delete'] || 'Delete',
-    btnCancelIcon:  '',
-    btnCancelClass: 'btn-default',
-    btnCancelLabel: Trestle.i18n['admin.confirmation.cancel'] || 'Cancel',
-    copyAttributes: ''
-  });
+  // Delete confirmation
+  new $.fn.confirmation.Constructor(root, Trestle.confirmation.DELETE);
+
+  // General confirmation
+  new $.fn.confirmation.Constructor(root, Trestle.confirmation.CONFIRM);
 });
