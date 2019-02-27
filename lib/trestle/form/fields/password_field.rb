c08e49a31e3852f11d4fbb60246f490e07b63630
Sam Pohlenz <sam@sampohlenz.com>
2019-02-27 11:52:06 +1030

    Set autocomplete="new-password" attribute on password fields

--- lib/trestle/form/fields/password_field.rb
+++ lib/trestle/form/fields/password_field.rb
@@ -2,6 +2,10 @@ class Trestle::Form::Fields::PasswordField < Trestle::Form::Fields::FormControl
   def field
     builder.raw_password_field(name, options)
   end
+
+  def defaults
+    super.merge(autocomplete: "new-password")
+  end
 end
 
 Trestle::Form::Builder.register(:password_field, Trestle::Form::Fields::PasswordField)
