90bed8a3393955762b45e44957515e3fb9b7a9ff
Sam Pohlenz <sam@sampohlenz.com>
2019-03-28 13:34:26 +1030

    JS indentation fix

--- app/assets/javascripts/trestle/components/_confirmation.js
+++ app/assets/javascripts/trestle/components/_confirmation.js
@@ -14,18 +14,18 @@ Trestle.ready(function() {
     btnCancelLabel: Trestle.i18n['trestle.confirmation.cancel'] || 'Cancel',
     copyAttributes: ''
   };
-  
+
   Trestle.confirmation.CONFIRM = $.extend({}, Trestle.confirmation.DEFAULTS, {
     selector: '[data-toggle="confirm"]'
   });
-  
+
   Trestle.confirmation.DELETE = $.extend({}, Trestle.confirmation.DEFAULTS, {
     selector:   '[data-toggle="confirm-delete"]',
     btnOkClass: 'btn-danger',
     btnOkLabel: Trestle.i18n['trestle.confirmation.delete'] || 'Delete'
   });
-  
-    // This must be bound to an element beneath document so
+
+  // This must be bound to an element beneath document so
   // that it is fired before any jquery_ujs events.
   var root = $('body');
 
