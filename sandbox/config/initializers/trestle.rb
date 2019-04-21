4cd007a7da739685f0d290699c85dcb89b0d6e7c
Sam Pohlenz <sam@sampohlenz.com>
2019-04-21 17:21:00 +0930

    Update views to use Boostrap 4 classes

--- sandbox/config/initializers/trestle.rb
+++ sandbox/config/initializers/trestle.rb
@@ -3,8 +3,8 @@ Trestle.configure do |config|
 
   config.menu do
     group "Badges & Labels" do
-      item "Item with counter", "#", badge: { text: (1..100).to_a.sample, class: "label-primary label-pill" }, priority: :first
-      item "Item with badge", "#", badge: { text: "NEW!", class: "label-success" }, icon: "fa fa-car"
+      item "Item with counter", "#", badge: { text: (1..100).to_a.sample, class: "badge-primary badge-pill" }, priority: :first
+      item "Item with badge", "#", badge: { text: "NEW!", class: "badge-success" }, icon: "fa fa-car"
     end
   end
 end
