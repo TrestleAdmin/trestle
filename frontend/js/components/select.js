044b008e2ce198edaa2791638ff11685e5d4d641
Sam Pohlenz <sam@sampohlenz.com>
2019-05-03 17:06:44 +0930

    Improve initialization of Select2 defaults

--- frontend/js/components/select.js
+++ frontend/js/components/select.js
@@ -2,14 +2,17 @@ import $ from 'jquery'
 
 import { init } from '../core/events'
 
+// Use Bootstrap theme
+$.fn.select2.defaults.set('theme', 'bootstrap')
+
+// Copy all classes from select tag to replacement select field (.select2-selection)
+$.fn.select2.defaults.set('containerCssClass', ':all:')
+
+// Copy all classes excluding 'form-control' from select tag to dropdown container (.select2-dropdown)
+$.fn.select2.defaults.set('dropdownCssClass', function (el) {
+  return el[0].className.replace(/\s*form-control\s*/, '')
+})
+
 init(function (root) {
-  $(root).find('select[data-enable-select2]').each(function () {
-    $(this).select2({
-      theme: 'bootstrap',
-      containerCssClass: ':all:',
-      dropdownCssClass: function (el) {
-        return el[0].className.replace(/\s*form-control\s*/, '')
-      }
-    })
-  })
+  $(root).find('select[data-enable-select2]').select2()
 })
