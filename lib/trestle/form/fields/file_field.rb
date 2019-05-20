6f95f2a1f0c3ce887961803d7fe4d47d629e1c91
Sam Pohlenz <sam@sampohlenz.com>
2019-05-20 10:47:08 +0930

    Add support for Bootstrap 4 custom file fields

--- lib/trestle/form/fields/file_field.rb
+++ lib/trestle/form/fields/file_field.rb
@@ -3,7 +3,26 @@ module Trestle
     module Fields
       class FileField < Field
         def field
-          builder.raw_file_field(name, options)
+          if custom?
+            content_tag(:div, class: "custom-file") do
+              concat builder.raw_file_field(name, options.merge(class: "custom-file-input"))
+              concat builder.label(name, choose_file_text, class: "custom-file-label", data: { browse: browse_text })
+            end
+          else
+            builder.raw_file_field(name, options)
+          end
+        end
+
+        def custom?
+          options[:custom] != false
+        end
+
+        def choose_file_text
+          I18n.t("trestle.file.choose_file", default: "Choose file")
+        end
+
+        def browse_text
+          I18n.t("trestle.file.browse", default: "Browse")
         end
       end
     end
