96d9a47fdf7ea2b8a7efb93eeb386565e715ee94
Sam Pohlenz <sam@sampohlenz.com>
2019-05-20 09:52:59 +0930

    Add support for Bootstrap 4 custom range fields

--- lib/trestle/form/fields/range_field.rb
+++ lib/trestle/form/fields/range_field.rb
@@ -5,6 +5,16 @@ module Trestle
         def field
           builder.raw_range_field(name, options)
         end
+
+        def custom?
+          options[:custom] != false
+        end
+
+        def normalize_options!
+          options.reverse_merge!(class: ["custom-range"]) if custom?
+
+          super
+        end
       end
     end
   end
