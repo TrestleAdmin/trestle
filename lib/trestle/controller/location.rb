4a8006ec9c3a3a771cf5c3bdb403e092c268a3bf
Sam Pohlenz <sam@sampohlenz.com>
2019-08-30 11:28:23 +0930

    Do not set X-Trestle-Location header when Location header is set

--- lib/trestle/controller/location.rb
+++ lib/trestle/controller/location.rb
@@ -4,11 +4,15 @@ module Trestle
       extend ActiveSupport::Concern
 
       included do
-        after_action :set_trestle_location_header, unless: :dialog_request?
+        after_action :set_trestle_location_header
       end
 
+      # The X-Trestle-Location header is set to indicate that the remote form has triggered
+      # a new page URL (e.g. new -> show) without demanding a full page refresh.
       def set_trestle_location_header
-        headers["X-Trestle-Location"] = request.path
+        unless dialog_request? || response.location
+          headers["X-Trestle-Location"] = request.path
+        end
       end
     end
   end
