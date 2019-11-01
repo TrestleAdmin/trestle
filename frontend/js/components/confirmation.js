e17ecb651f6719924df4ebdf25c7610dd5ef5e63
Sam Pohlenz <sam@sampohlenz.com>
2019-11-01 10:57:44 +1030

    Add data-remote to list of copied attributes for confirmation popover

--- frontend/js/components/confirmation.js
+++ frontend/js/components/confirmation.js
@@ -12,7 +12,7 @@ init(function (root) {
     btnOkLabel: i18n['trestle.confirmation.ok'] || 'OK',
     btnCancelClass: 'btn btn-sm btn-light',
     btnCancelLabel: i18n['trestle.confirmation.cancel'] || 'Cancel',
-    copyAttributes: 'href target data-method data-url data-params data-type'
+    copyAttributes: 'href target data-remote data-method data-url data-params data-type'
   }
 
   const CONFIRM = { ...DEFAULTS,
