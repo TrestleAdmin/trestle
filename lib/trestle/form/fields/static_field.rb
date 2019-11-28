a743520eb74bc7ec593ffe9fa9b762c2a69a50a3
Sam Pohlenz <sam@sampohlenz.com>
2019-11-28 20:59:28 +1030

    Extract #default_value method for static fields

--- lib/trestle/form/fields/static_field.rb
+++ lib/trestle/form/fields/static_field.rb
@@ -10,7 +10,6 @@ module Trestle
           else
             @value = value
           end
-          @value ||= builder.object.send(name) if builder.object
 
           super(builder, template, name, options, &block)
         end
@@ -19,8 +18,12 @@ module Trestle
           if block
             template.capture(&block)
           else
-            content_tag(:p, value, class: "form-control-static")
+            content_tag(:p, value || default_value, class: "form-control-static")
           end
+        end
+
+        def default_value
+          builder.object.send(name) if builder.object
         end
       end
     end
