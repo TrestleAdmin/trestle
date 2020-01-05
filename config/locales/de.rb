1771453d9a55d3ddc514cb225ad7dca623656d81
Mitosch <mschindowski@gmail.com>
2020-01-05 09:19:52 +0100

    add german translations

---
+++ config/locales/de.rb
@@ -0,0 +1,18 @@
+{
+  de: {
+    date: {
+      formats: {
+        trestle_date: "%-d. %b. %Y",
+        trestle_calendar: "%d.%m.%Y"
+      }
+    },
+
+    time: {
+      formats: {
+        trestle_date: "%-d. %b. %Y",
+        trestle_time: "%H:%M",
+        trestle_time_with_seconds: "%H:%M:%S"
+      }
+    }
+  }
+}
