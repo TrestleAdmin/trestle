5a5b6dc523286964f9582a89d675091b499d35d8
Sam Pohlenz <sam@sampohlenz.com>
2020-01-23 16:36:59 +1030

    Don't set i18n translations for JS if no translation available

--- app/helpers/trestle/i18n_helper.rb
+++ app/helpers/trestle/i18n_helper.rb
@@ -11,10 +11,14 @@ module Trestle
       end
     end
 
-    def default_translation(key)
-      translate(key, locale: :en)
-    rescue I18n::InvalidLocale
-      key.split(".").last.humanize
+    def i18n_javascript_translations
+      Trestle.config.javascript_i18n_keys.map { |key|
+        begin
+          [key, t(key, raise: true)]
+        rescue I18n::MissingTranslationData
+          nil
+        end
+      }.compact
     end
   end
 end
