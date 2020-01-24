cd2185d71b1ff4b3a0cd3c1d04aed3ae8fe19ff8
Sam Pohlenz <sam@sampohlenz.com>
2020-01-24 21:25:54 +1030

    Add --singular option to trestle:resource generator

--- lib/generators/trestle/resource/resource_generator.rb
+++ lib/generators/trestle/resource/resource_generator.rb
@@ -3,10 +3,24 @@ module Trestle
     class ResourceGenerator < ::Rails::Generators::NamedBase
       desc "Creates a Trestle admin resource"
 
+      class_option :singular, type: :boolean, default: false, desc: "Generate a singular resource"
+
       source_root File.expand_path("../templates", __FILE__)
 
       def create_admin
-        template "admin.rb.erb", File.join("app/admin", class_path, "#{plural_name}_admin.rb")
+        template "admin.rb.erb", File.join("app/admin", class_path, "#{admin_name}_admin.rb")
+      end
+
+      def admin_name
+        singular? ? singular_name : plural_name
+      end
+
+      def parameter_name
+        singular_name.singularize
+      end
+
+      def singular?
+        options[:singular]
       end
 
     protected
--- spec/generators/resource_generator_spec.rb
+++ spec/generators/resource_generator_spec.rb
@@ -19,6 +19,26 @@ describe Trestle::Generators::ResourceGenerator, type: :generator do
 
       it { is_expected.to exist }
       it { is_expected.to contain "Trestle.resource(:posts) do" }
+      it { is_expected.to contain "table do" }
+      it { is_expected.to contain "form do |post|"}
+    end
+  end
+
+  context "with a singular resource" do
+    describe "the generated files" do
+      before do
+        run_generator %w(account --singular)
+      end
+
+      describe "the admin resource" do
+        subject { file("app/admin/account_admin.rb") }
+
+        it { is_expected.to exist }
+        it { is_expected.to contain "Trestle.resource(:account, singular: true) do" }
+        it { is_expected.to contain "instance do" }
+        it { is_expected.not_to contain "table do" }
+        it { is_expected.to contain "form do |account|"}
+      end
     end
   end
 
@@ -33,6 +53,26 @@ describe Trestle::Generators::ResourceGenerator, type: :generator do
 
         it { is_expected.to exist }
         it { is_expected.to contain "Trestle.resource(:posts, scope: Namespace) do" }
+        it { is_expected.to contain "table do" }
+        it { is_expected.to contain "form do |post|"}
+      end
+    end
+
+    context "with a singular resource" do
+      describe "the generated files" do
+        before do
+          run_generator %w(Namespace::Account --singular)
+        end
+
+        describe "the admin resource" do
+          subject { file("app/admin/namespace/account_admin.rb") }
+
+          it { is_expected.to exist }
+          it { is_expected.to contain "Trestle.resource(:account, scope: Namespace, singular: true) do" }
+          it { is_expected.to contain "instance do" }
+          it { is_expected.not_to contain "table do" }
+          it { is_expected.to contain "form do |account|"}
+        end
       end
     end
   end
