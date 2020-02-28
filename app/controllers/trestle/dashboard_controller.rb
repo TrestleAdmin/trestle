33eea0415852c8ed9817284c0e494776465acec4
Sam Pohlenz <sam@sampohlenz.com>
2020-02-28 13:27:24 +1030

    Use consistent class style in dashboard controller

--- app/controllers/trestle/dashboard_controller.rb
+++ app/controllers/trestle/dashboard_controller.rb
@@ -1,15 +1,17 @@
-class Trestle::DashboardController < Trestle::ApplicationController
-  def index
-    admin = primary_admin
-    redirect_to admin.path if admin
-  end
+module Trestle
+  class DashboardController < ApplicationController
+    def index
+      admin = primary_admin
+      redirect_to admin.path if admin
+    end
 
-private
-  def primary_admin
-    if navigation = Trestle.navigation(self).first
-      navigation.admin
-    elsif Trestle.admins.values.any?
-      Trestle.admins.values.first
+  private
+    def primary_admin
+      if navigation = Trestle.navigation(self).first
+        navigation.admin
+      elsif Trestle.admins.values.any?
+        Trestle.admins.values.first
+      end
     end
   end
 end
