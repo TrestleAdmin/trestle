ce0b6e271d4e61e5f5808c0ddeaf5d077588f2e7
Sam Pohlenz <sam@sampohlenz.com>
2020-10-14 16:49:04 +1030

    Add specs for range fields

---
+++ spec/trestle/form/fields/range_field_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+require_relative "form_field_examples"
+
+describe Trestle::Form::Fields::RangeField, type: :helper do
+  include_context "form", :level, 10
+
+  it_behaves_like "a form field", :level
+
+  subject { builder.range_field(:level, options) }
+
+  it "renders a custom file field by default" do
+    expect(subject).to have_tag("input.custom-range", type: "range", id: "article_level")
+  end
+
+  context "when options[:custom] is set to false" do
+    let(:options) { { custom: false } }
+
+    it "renders a regular file field" do
+      expect(subject).to have_tag("input", type: "range", without: { class: "custom-range" })
+    end
+  end
+end
