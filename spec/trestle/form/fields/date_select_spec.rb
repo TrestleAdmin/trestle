caac53dbfca30755bd4784d30393ccee90a3fe41
Sam Pohlenz <sam@sampohlenz.com>
2020-10-16 15:43:49 +1030

    Add specs for date_select, time_select and datetime_select

---
+++ spec/trestle/form/fields/date_select_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+require_relative "form_field_examples"
+
+describe Trestle::Form::Fields::DateSelect, type: :helper do
+  include_context "form", :published_on, Date.new(2020, 10, 16)
+
+  let(:html_options) { {} }
+
+  subject { builder.date_select :published_on, options, html_options }
+
+  it_behaves_like "a form field", :published_on
+
+  it "renders a date select within a .date-select container" do
+    expect(subject).to have_tag(".date-select") do
+      with_tag "select.form-control", with: { name: "article[published_on(1i)]", "data-enable-select2": true } do
+        with_tag "option[selected]", with: { value: 2020 }
+      end
+
+      with_tag "select.form-control", with: { name: "article[published_on(2i)]", "data-enable-select2": true } do
+        with_tag "option[selected]", with: { value: 10 }
+      end
+
+      with_tag "select.form-control", with: { name: "article[published_on(3i)]", "data-enable-select2": true } do
+        with_tag "option[selected]", with: { value: 16 }
+      end
+    end
+  end
+
+  context "when options[:disabled] is set to true" do
+    let(:options) { { disabled: true } }
+
+    it "sets the disabled attribute on the select controls" do
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_on(1i)]", "data-enable-select2": true })
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_on(2i)]", "data-enable-select2": true })
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_on(3i)]", "data-enable-select2": true })
+    end
+  end
+
+  context "when options[:readonly] is set to true" do
+    let(:options) { { readonly: true } }
+
+    it "sets the disabled attribute on the select controls" do
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_on(1i)]", "data-enable-select2": true })
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_on(2i)]", "data-enable-select2": true })
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_on(3i)]", "data-enable-select2": true })
+    end
+  end
+end
---
+++ spec/trestle/form/fields/time_select_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+require_relative "form_field_examples"
+
+describe Trestle::Form::Fields::TimeSelect, type: :helper do
+  include_context "form", :published_at, Time.new(2020, 10, 16, 14, 30)
+
+  let(:html_options) { {} }
+
+  subject { builder.time_select :published_at, options, html_options }
+
+  it_behaves_like "a form field", :published_at
+
+  it "renders a date select within a .time-select container" do
+    expect(subject).to have_tag(".time-select") do
+      with_tag "select.form-control", with: { name: "article[published_at(4i)]", "data-enable-select2": true } do
+        with_tag "option[selected]", with: { value: 14 }
+      end
+
+      with_tag "select.form-control", with: { name: "article[published_at(5i)]", "data-enable-select2": true } do
+        with_tag "option[selected]", with: { value: 30 }
+      end
+    end
+  end
+
+  context "when options[:disabled] is set to true" do
+    let(:options) { { disabled: true } }
+
+    it "sets the disabled attribute on the select controls" do
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_at(4i)]", "data-enable-select2": true })
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_at(5i)]", "data-enable-select2": true })
+    end
+  end
+
+  context "when options[:readonly] is set to true" do
+    let(:options) { { readonly: true } }
+
+    it "sets the disabled attribute on the select controls" do
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_at(4i)]", "data-enable-select2": true })
+      expect(subject).to have_tag('select[disabled]', with: { name: "article[published_at(5i)]", "data-enable-select2": true })
+    end
+  end
+end
