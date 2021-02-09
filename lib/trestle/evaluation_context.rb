c52241aa899b39855f93593f7d39d6706884edfd
Sam Pohlenz <sam@sampohlenz.com>
2021-02-09 14:49:47 +1030

    Restore compatibility with Ruby 2.6

--- lib/trestle/evaluation_context.rb
+++ lib/trestle/evaluation_context.rb
@@ -5,20 +5,28 @@ module Trestle
   # both the Adapter/Navigation instance, as well as the controller/view from where they are invoked.
   module EvaluationContext
   protected
+    def self.ruby2_keywords(*)
+    end unless respond_to?(:ruby2_keywords, true)
+
     # Missing methods are called on the given context if available.
     #
     # We include private methods as methods such as current_user
     # are usually declared as private or protected.
-    def method_missing(name, *args, **kwargs, &block)
-      if @context && @context.respond_to?(name, true)
-        @context.send(name, *args, **kwargs, &block)
+    ruby2_keywords def method_missing(name, *args, &block)
+      if context_responds_to?(name)
+        @context.send(name, *args, &block)
       else
         super
       end
     end
 
     def respond_to_missing?(name, include_private=false)
-      (@context && @context.respond_to?(name, true)) || super
+      context_responds_to?(name) || super
+    end
+
+  private
+    def context_responds_to?(name)
+      @context && @context.respond_to?(name, true)
     end
   end
 end
