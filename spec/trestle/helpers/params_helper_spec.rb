600d5b435b15911b16a233e57c196d0389017773
Sam Pohlenz <sam@sampohlenz.com>
2021-02-22 17:27:41 +1030

    Only support persistent_params hash declaration on Rails >= 5.1

--- spec/trestle/helpers/params_helper_spec.rb
+++ spec/trestle/helpers/params_helper_spec.rb
@@ -12,9 +12,9 @@ describe Trestle::ParamsHelper do
       expect(persistent_params).to eq(ActionController::Parameters.new(sort: :field, order: "asc").permit!)
     end
 
-    context "when Trestle.config.persistent_params contains hashes and arrays" do
+    context "when Trestle.config.persistent_params contains an array declaration" do
       before(:each) do
-        expect(Trestle.config).to receive(:persistent_params).and_return([:sort, { hash: {} }, :order, { array: [] }])
+        expect(Trestle.config).to receive(:persistent_params).and_return([:sort, { array: [] }, :order])
       end
 
       let(:params) do
@@ -22,15 +22,35 @@ describe Trestle::ParamsHelper do
           sort: :field,
           order: "asc",
           ignore: "me",
-          hash: { key: "value" },
-          ignore_hash: { abc: "123" },
           array: ["foo", "bar", "baz"],
           ignore_array: ["one", "two"]
         })
       end
 
-      it "includes persistent hash and array params" do
-        expect(persistent_params).to eq(ActionController::Parameters.new(sort: :field, order: "asc", hash: { key: "value" }, array: ["foo", "bar", "baz"]).permit!)
+      it "includes the persistent array params" do
+        expect(persistent_params).to eq(ActionController::Parameters.new(sort: :field, order: "asc", array: ["foo", "bar", "baz"]).permit!)
+      end
+    end
+
+    if Rails.gem_version > Gem::Version.new("5.1")
+      context "when Trestle.config.persistent_params contains a hash declaration" do
+        before(:each) do
+          expect(Trestle.config).to receive(:persistent_params).and_return([:sort, { hash: {} }, :order])
+        end
+
+        let(:params) do
+          ActionController::Parameters.new({
+            sort: :field,
+            order: "asc",
+            ignore: "me",
+            hash: { key: "value" },
+            ignore_hash: { abc: "123" }
+          })
+        end
+
+        it "includes persistent hash param" do
+          expect(persistent_params).to eq(ActionController::Parameters.new(sort: :field, order: "asc", hash: { key: "value" }).permit!)
+        end
       end
     end
   end
