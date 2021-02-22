0bae00ece052bfa74b10e3386ea9ab4ebc953c0e
Sam Pohlenz <sam@sampohlenz.com>
2021-02-22 17:01:27 +1030

    Add support for hashes and arrays in config.persistent_params

--- app/helpers/trestle/params_helper.rb
+++ app/helpers/trestle/params_helper.rb
@@ -1,7 +1,10 @@
 module Trestle
   module ParamsHelper
     def persistent_params
-      params.slice(*Trestle.config.persistent_params).permit(*Trestle.config.persistent_params)
+      flat, nested = Trestle.config.persistent_params.partition { |p| !p.is_a?(Hash) }
+      nested = nested.inject({}) { |result, param| result.merge(param) }
+
+      params.slice(*(flat + nested.keys)).permit(*(flat << nested))
     end
   end
 end
--- spec/trestle/helpers/params_helper_spec.rb
+++ spec/trestle/helpers/params_helper_spec.rb
@@ -4,12 +4,34 @@ require_relative '../../../app/helpers/trestle/params_helper'
 
 describe Trestle::ParamsHelper do
   include Trestle::ParamsHelper
-  
+
   let(:params) { ActionController::Parameters.new(sort: :field, order: "asc", ignore: "me") }
 
   describe "#persistent_params" do
     it "only returns params that should be persistent" do
       expect(persistent_params).to eq(ActionController::Parameters.new(sort: :field, order: "asc").permit!)
+    end
+
+    context "when Trestle.config.persistent_params contains hashes and arrays" do
+      before(:each) do
+        expect(Trestle.config).to receive(:persistent_params).and_return([:sort, { hash: {} }, :order, { array: [] }])
+      end
+
+      let(:params) do
+        ActionController::Parameters.new({
+          sort: :field,
+          order: "asc",
+          ignore: "me",
+          hash: { key: "value" },
+          ignore_hash: { abc: "123" },
+          array: ["foo", "bar", "baz"],
+          ignore_array: ["one", "two"]
+        })
+      end
+
+      it "includes persistent hash and array params" do
+        expect(persistent_params).to eq(ActionController::Parameters.new(sort: :field, order: "asc", hash: { key: "value" }, array: ["foo", "bar", "baz"]).permit!)
+      end
     end
   end
 end
