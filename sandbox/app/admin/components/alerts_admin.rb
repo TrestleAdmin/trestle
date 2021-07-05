ddb6a9dae9287d42e468ad2f4aebfe6ed5a3fedf
Sam Pohlenz <sam@sampohlenz.com>
2021-07-05 17:05:45 +0930

    Add sandbox page for supported alerts

---
+++ sandbox/app/admin/components/alerts_admin.rb
@@ -0,0 +1,7 @@
+Trestle.admin(:alerts, scope: Components) do
+  menu do
+    group :components do
+      item :alerts, icon: "fas fa-exclamation-circle", priority: 1
+    end
+  end
+end
--- sandbox/app/admin/components/typography_admin.rb
+++ sandbox/app/admin/components/typography_admin.rb
@@ -1,7 +1,7 @@
 Trestle.admin(:typography, scope: Components) do
   menu do
     group :components do
-      item :typography, icon: "fas fa-font"
+      item :typography, icon: "fas fa-font", priority: 0
     end
   end
 end
