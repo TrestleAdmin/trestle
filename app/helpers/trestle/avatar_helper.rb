f60e1aa4668074e481849d08ff3003215542d0a1
Sam Pohlenz <sam@sampohlenz.com>
2021-07-09 20:07:15 +0930

    Add sandbox sections for Avatars and Timestamps to Miscellaneous page

--- app/helpers/trestle/avatar_helper.rb
+++ app/helpers/trestle/avatar_helper.rb
@@ -3,19 +3,23 @@ module Trestle
     def avatar(options={})
       fallback = options.delete(:fallback) if options[:fallback]
 
-      content_tag(:div, options.reverse_merge(class: "avatar")) do
+      content_tag(:div, default_avatar_options.merge(options)) do
         concat content_tag(:span, fallback, class: "avatar-fallback") if fallback
         concat yield if block_given?
       end
     end
 
     def gravatar(email, options={})
-      options = { d: "mm" }.merge(options)
+      options = { d: "mp" }.merge(options)
 
       url = "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.to_s.downcase)}.png"
       url << "?#{options.to_query}" if options.any?
 
       image_tag(url)
+    end
+
+    def default_avatar_options
+      Trestle::Options.new(class: ["avatar"])
     end
   end
 end
