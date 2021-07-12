f797f2c008e976755fcd37b2c1943b19ab27f469
Sam Pohlenz <sam@sampohlenz.com>
2021-07-12 15:00:00 +0930

    Move sandbox badges navigation group to end

--- sandbox/config/initializers/trestle.rb
+++ sandbox/config/initializers/trestle.rb
@@ -2,8 +2,8 @@ Trestle.configure do |config|
   config.site_title = "Trestle Sandbox"
 
   config.menu do
-    group "Badges & Labels" do
-      item "Item with counter", "#", badge: { text: (1..100).to_a.sample, class: "badge-primary badge-pill" }, priority: :first
+    group "Badges & Labels", priority: :last do
+      item "Item with counter", "#", badge: { text: 99, class: "badge-primary badge-pill" }, priority: :first
       item "Item with badge", "#", badge: { text: "NEW!", class: "badge-success" }, icon: "fa fa-car"
     end
   end
