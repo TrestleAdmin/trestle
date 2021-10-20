b6f813c6e75471202dc76b88593c1ac109206d65
Sam Pohlenz <sam@sampohlenz.com>
2021-10-20 20:24:53 +1030

    Add custom rendering for enums in automatic tables

--- lib/trestle/table/automatic.rb
+++ lib/trestle/table/automatic.rb
@@ -14,6 +14,13 @@ module Trestle
           case attribute.type
           when :association
             Column.new(attribute.association_name, sort: false)
+          when :enum
+            Column.new(attribute.name, align: :center, sort: attribute.name) do |instance|
+              if value = instance.public_send(attribute.name)
+                full_attribute_name = [attribute.name.to_s.pluralize, value].join(".")
+                status_tag(admin.human_attribute_name(full_attribute_name, default: value.humanize))
+              end
+            end
           else
             Column.new(attribute.name, link: index.zero?, align: (:center if [:datetime, :boolean].include?(attribute.type)))
           end
