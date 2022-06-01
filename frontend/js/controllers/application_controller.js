4093c479f52c1be764259db8925dc3cef68b076d
Sam Pohlenz <sam@sampohlenz.com>
2022-06-01 16:29:58 +0930

    Add csrfToken getter method to Stimulus ApplicationController

--- frontend/js/controllers/application_controller.js
+++ frontend/js/controllers/application_controller.js
@@ -1,4 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  get csrfToken () {
+    return document.querySelector("[name='csrf-token']").content
+  }
 }
