a32992aac4752116acd9d7b75cc1886f3b83b9d9
Mike Blom <mblom@instructure.com>
2022-06-15 10:34:02 -0600

    MCE-15901 Cleanup plurality bug

--- lib/trestle/admin.rb
+++ lib/trestle/admin.rb
@@ -27,7 +27,7 @@ module Trestle
       attr_accessor :form
       attr_accessor :remove_form_attributes
       attr_accessor :remove_table_attributes
-      attr_accessor :remove_select_attribute
+      attr_accessor :remove_select_attributes
 
       attr_writer :options
       attr_writer :breadcrumb
