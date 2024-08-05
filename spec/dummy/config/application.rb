5e406540629bbbb9862f5472a2ec995181ea3e3b
Sam Pohlenz <sam@sampohlenz.com>
2024-08-05 11:44:00 +0930

    Require ActiveJob railtie in spec dummy app (until hotwired/turbo-rails#602)

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,8 +4,8 @@ require_relative 'boot'
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
+require "active_job/railtie"
 # require "action_mailer/railtie"
-# require "active_job/railtie"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
