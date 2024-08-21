632ca140abf433cf318cde7aaeccc8830fbfa936
Sam Pohlenz <sam@sampohlenz.com>
2024-08-21 17:23:36 +0930

    Minimize mocking in FormatHelper spec

--- spec/trestle/helpers/format_helper_spec.rb
+++ spec/trestle/helpers/format_helper_spec.rb
@@ -5,55 +5,45 @@ require_relative '../../../app/helpers/trestle/format_helper'
 describe Trestle::FormatHelper do
   include Trestle::FormatHelper
 
-  before(:each) do
-    allow(self).to receive(:truncate) { |v| v }
-  end
+  include Trestle::DisplayHelper
+  include Trestle::IconHelper
+  include Trestle::StatusHelper
+  include Trestle::TimestampHelper
+
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::DateHelper
+  include ActionView::Helpers::NumberHelper
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::TranslationHelper
 
   describe "#format_value" do
     it "automatically formats timestamp values" do
       time = Time.now
-      timestamp = double
-
-      expect(self).to receive(:timestamp).with(time).and_return(timestamp)
-      expect(format_value(time)).to eq(timestamp)
+      expect(format_value(time)).to eq(timestamp(time))
     end
 
     it "automatically formats date values" do
       date = Date.today
-      datestamp = double
-
-      expect(self).to receive(:datestamp).with(date).and_return(datestamp)
-      expect(format_value(date)).to eq(datestamp)
+      expect(format_value(date)).to eq(datestamp(date))
     end
 
     it "returns 'none' text for nil values" do
-      blank = double
-
-      expect(self).to receive(:content_tag).with(:span, "None", class: "blank").and_return(blank)
-      expect(format_value(nil)).to eq(blank)
+      result = format_value(nil)
+      expect(result).to have_tag("span.blank", text: "None")
     end
 
     it "returns custom blank text when :blank String option provided" do
-      blank = double
-
-      expect(self).to receive(:content_tag).with(:span, "Empty", class: "blank").and_return(blank)
-      expect(format_value(nil, blank: "Empty")).to eq(blank)
+      result = format_value(nil, blank: "Empty")
+      expect(result).to have_tag("span.blank", text: "Empty")
     end
 
     it "calls custom blank block when :blank option is callable" do
-      blank = double
-
-      expect(self).to receive(:icon).with("fa fa-ban").and_return(blank)
-      expect(format_value(nil, blank: -> { icon("fa fa-ban") })).to eq(blank)
+      result = format_value(nil, blank: -> { icon("fa fa-ban") })
+      expect(result).to have_tag("i.fa.fa-ban")
     end
 
     it "automatically formats true values" do
-      status = double
-      icon = double
-
-      expect(self).to receive(:icon).with("fa fa-check").and_return(icon)
-      expect(self).to receive(:status_tag).with(icon, :success).and_return(status)
-      expect(format_value(true)).to eq(status)
+      expect(format_value(true)).to eq(status_tag(icon("fa fa-check"), :success))
     end
 
     it "leaves false values empty" do
@@ -61,41 +51,30 @@ describe Trestle::FormatHelper do
     end
 
     it "calls display for model-like values" do
-      representation = double
-      model = double(id: "123")
-
-      expect(self).to receive(:display).with(model).and_return(representation)
-      expect(format_value(model)).to eq(representation)
+      model = double(id: "123", display_name: "Display 123")
+      expect(format_value(model)).to eq("Display 123")
     end
 
     it "automatically formats array values" do
-      list, items = double, double
-      first_item, second_item = double, double
+      result = format_value(["First", "Second"])
 
-      expect(self).to receive(:content_tag).with(:li, "First").and_return(first_item)
-      expect(self).to receive(:content_tag).with(:li, "Second").and_return(second_item)
-      expect(self).to receive(:safe_join).with([first_item, second_item], "\n").and_return(items)
-      expect(self).to receive(:content_tag).with(:ol, items).and_return(list)
-      expect(format_value(["First", "Second"])).to eq(list)
+      expect(result).to have_tag("ol") do
+        with_tag "li", text: "First"
+        with_tag "li", text: "Second"
+      end
     end
 
     it "formats value as currency" do
-      currency = double
-
-      expect(self).to receive(:number_to_currency).with(123.45).and_return(currency)
-      expect(format_value(123.45, format: :currency)).to eq(currency)
+      expect(format_value(123.45, format: :currency)).to eq("$123.45")
     end
 
     it "formats values as tags" do
-      tags = double
-      list = double
-      first_tag, second_tag = double, double
+      result = format_value(["First", "Second"], format: :tags)
 
-      expect(self).to receive(:content_tag).with(:span, "First", class: "tag tag-primary").and_return(first_tag)
-      expect(self).to receive(:content_tag).with(:span, "Second", class: "tag tag-primary").and_return(second_tag)
-      expect(self).to receive(:safe_join).with([first_tag, second_tag]).and_return(tags)
-      expect(self).to receive(:content_tag).with(:div, tags, class: "tag-list").and_return(list)
-      expect(format_value(["First", "Second"], format: :tags)).to eq(list)
+      expect(result).to have_tag("div.tag-list") do
+        with_tag "span.tag.tag-primary", text: "First"
+        with_tag "span.tag.tag-primary", text: "Second"
+      end
     end
   end
 end
