c3967b18e18f01890201685f3f6d1927c9c00b48
Sam Pohlenz <sam@sampohlenz.com>
2024-08-25 13:01:11 +0930

    Add spec and docs for ModalHelper

--- app/helpers/trestle/modal_helper.rb
+++ app/helpers/trestle/modal_helper.rb
@@ -1,16 +1,34 @@
 module Trestle
   module ModalHelper
+    # Merges the given options with the existing hash of defined modal options.
+    #
+    # Trestle uses Bootstrap modal markup (https://getbootstrap.com/docs/5.3/components/modal/)
+    # to render modals, which consist of an outer .modal wrapper div with an inner
+    # .modal-dialog div.
+    #
+    # The `class` attribute (e.g. `"modal-lg"`) is applied to the inner element, and all
+    # other attributes are applied to the outer element. A class can be applied to the
+    # wrapper element using the `wrapper_class` option.
+    #
+    # Examples
+    #
+    #   <% modal_options! class: "modal-lg", controller: "modal-stimulus" %>
+    #
+    #   <% modal_options! wrapper_class: "custom-modal-animation" %>
+    #
     def modal_options!(options)
       modal_options.merge!(options)
     end
 
+    # Returns a hash of the currently defined modal options
     def modal_options
       @_modal_options ||= {}
     end
 
+    # Returns the HTML attributes to apply to the modal wrapper (.modal) <div>
     def modal_wrapper_attributes
       {
-        class: ["modal", "fade", modal_options[:wrapper_class]],
+        class: ["modal", "fade", modal_options[:wrapper_class]].compact,
         tabindex: "-1",
         role: "dialog",
         data: {
@@ -19,9 +37,10 @@ module Trestle
       }.deep_merge(modal_options.except(:class, :wrapper_class, :controller))
     end
 
+    # Returns the HTML attributes to apply to the inner modal dialog (.modal-dialog) <div>
     def modal_dialog_attributes
       {
-        class: ["modal-dialog", modal_options[:class]],
+        class: ["modal-dialog", modal_options[:class]].compact,
         role: "document"
       }
     end
---
+++ spec/trestle/helpers/modal_helper_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+require_relative '../../../app/helpers/trestle/modal_helper'
+
+describe Trestle::ModalHelper do
+  include Trestle::ModalHelper
+
+  describe "#modal_options!" do
+    it "merges hash with existing modal options" do
+      modal_options!(class: "modal-class")
+      modal_options!(id: "modal-id", controller: "modal-controller")
+
+      expect(modal_options).to eq({
+        class: "modal-class",
+        id: "modal-id",
+        controller: "modal-controller"
+      })
+    end
+  end
+
+  describe "#modal_wrapper_attributes" do
+    it "returns core HTML attributes for the modal wrapper element" do
+      expect(modal_wrapper_attributes).to eq({
+        class: ["modal", "fade"],
+        tabindex: "-1",
+        role: "dialog",
+        data: { controller: "modal" }
+      })
+    end
+
+    it "merges the wrapper class and id from modal_options if provided" do
+      modal_options!(wrapper_class: "custom-modal", id: "custom-modal-id")
+
+      expect(modal_wrapper_attributes).to eq({
+        class: ["modal", "fade", "custom-modal"],
+        tabindex: "-1",
+        role: "dialog",
+        data: { controller: "modal" },
+        id: "custom-modal-id"
+      })
+    end
+
+    it "merges teh controller from modal_options if provided" do
+      modal_options!(controller: "custom-modal")
+
+      expect(modal_wrapper_attributes).to eq({
+        class: ["modal", "fade"],
+        tabindex: "-1",
+        role: "dialog",
+        data: { controller: "modal custom-modal" }
+      })
+    end
+  end
+
+  describe "#modal_dialog_attributes" do
+    it "returns core HTML attributes for the modal-dialog element" do
+      expect(modal_dialog_attributes).to eq({ class: ["modal-dialog"], role: "document" })
+    end
+
+    it "merges the class from modal_options if provided" do
+      modal_options!(class: "modal-lg")
+      expect(modal_dialog_attributes).to eq({ class: ["modal-dialog", "modal-lg"], role: "document" })
+    end
+  end
+end
