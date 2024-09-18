ed160e4e3fe765969c3e03525202121b3d6ed0ca
Sam Pohlenz <sam@sampohlenz.com>
2024-09-18 15:45:48 +0930

    Add serialize_persistent_params helper method (#493)

--- app/helpers/trestle/params_helper.rb
+++ app/helpers/trestle/params_helper.rb
@@ -17,5 +17,26 @@ module Trestle
 
       params.slice(*(flat + nested.keys)).permit(*(flat << nested))
     end
+
+    # Converts the current set of persistent params into hidden form fields, suitable
+    # for inclusion within a form tag (e.g for search or filtering).
+    #
+    # except - List of param keys to exclude
+    # only   - List of param keys to only include
+    #
+    # Returns a HTML-safe String.
+    def serialize_persistent_params(except: [], only: [])
+      params = persistent_params
+
+      if Array(only).any?
+        params = params.slice(*only)
+      elsif Array(except).any?
+        params = params.except(*except)
+      end
+
+      safe_join(to_form_params(params).map { |param|
+        tag.input(type: "hidden", name: param[:name], value: param[:value], autocomplete: "off")
+      }, "\n")
+    end
   end
 end
