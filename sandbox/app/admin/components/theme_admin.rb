7146fc3dc01ff7d0c20d57e5416a9f29a7e6c5cf
Sam Pohlenz <sam@sampohlenz.com>
2024-09-23 20:19:06 +0930

    Remove block passing / layout support from 'trestle/flash/alert' partial

--- sandbox/app/admin/components/theme_admin.rb
+++ sandbox/app/admin/components/theme_admin.rb
@@ -4,4 +4,12 @@ Trestle.admin(:theme, scope: Components) do
       item :theme, icon: "fas fa-palette", priority: 8
     end
   end
+
+  helper do
+    def alert_message(html_class)
+      <<-HTML.html_safe
+        This custom alert has the <code>#{html_class}</code> class. This is the <a href="#">Link style</a>.
+      HTML
+    end
+  end
 end
