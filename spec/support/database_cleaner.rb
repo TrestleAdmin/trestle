abe87af00b9a3e277ca421371b77f71f4cd49686
Sam Pohlenz <sam@sampohlenz.com>
2024-12-02 13:20:25 +1030

    Minor tweaks to database cleaner configuration

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -11,15 +11,13 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system) do
     # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so we can use transaction strategy for speed.
+    # with the specs, so continue to use transaction strategy for speed.
     driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
 
-    if driver_shares_db_connection_with_specs
-      DatabaseCleaner.strategy = :transaction
-    else
-      # Non-:rack_test driver is probably a driver for a JavaScript browser
-      # with a Rack app under test that does *not* share a database
-      # connection with the specs, so we must use truncation strategy.
+    unless driver_shares_db_connection_with_specs
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
       DatabaseCleaner.strategy = :truncation
     end
   end
