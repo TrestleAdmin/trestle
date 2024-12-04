d5b7d3b2029fa1ae4d40a9dcd2c02f284fa788cc
Sam Pohlenz <sam@sampohlenz.com>
2024-12-04 15:28:10 +1030

    Pass element to actionsList method in Stimulus ApplicationController

--- frontend/js/controllers/application_controller.js
+++ frontend/js/controllers/application_controller.js
@@ -4,7 +4,7 @@ import { fetchWithErrorHandling, fetchTurboStream } from '../core/fetch'
 
 export default class extends Controller {
   appendAction (event, method, element = this.element) {
-    const actions = this.actionsList
+    const actions = this.actionsList(element)
     const newAction = `${event}->${this.identifier}#${method}`
 
     if (!actions.includes(newAction)) {
@@ -16,7 +16,7 @@ export default class extends Controller {
 
   removeAction (event, method, element = this.element) {
     const newAction = `${event}->${this.identifier}#${method}`
-    const actions = this.actionsList.filter(action => action !== newAction)
+    const actions = this.actionsList(element).filter(action => action !== newAction)
 
     element.dataset.action = actions.join(' ')
   }
@@ -29,15 +29,15 @@ export default class extends Controller {
     return fetchTurboStream(url, options)
   }
 
-  get csrfToken () {
-    return document.querySelector("[name='csrf-token']").content
-  }
-
-  get actionsList () {
+  actionsList (element) {
     if (element.dataset.action) {
       return element.dataset.action.split(' ')
     } else {
       return []
     }
+  }
+
+  get csrfToken () {
+    return document.querySelector("[name='csrf-token']").content
   }
 }
